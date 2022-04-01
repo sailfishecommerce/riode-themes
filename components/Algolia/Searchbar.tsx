172ad76d50cd6120175232fd98c8d929383892c8
BisiDavi <oludavidconnect@gmail.com>
2022-04-01 20:42:12 +0100

    menu updated

--- components/Algolia/Searchbar.tsx
+++ components/Algolia/Searchbar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Searchbar() {
   return (
-    <div className="searchbar flex flex-col w-3/6">
+    <div className="searchbar flex flex-col w-1/2">
       <input
         type="text"
         className="border-2 border-blue-700 rounded-lg ring-blue-700 h-10 px-2"
--- components/Menu/NavMenu.tsx
+++ components/Menu/NavMenu.tsx
@@ -4,7 +4,7 @@ import IconClasses from "@/lib/IconClasses";
 
 export default function NavMenu() {
   return (
-    <ul className="flex item-center w-1/4">
+    <ul className="flex item-center justify-center">
       {navMenuContent.map((navList: any, index: number) => {
         const iconPosition = navList.iconPosition?.includes("right")
           ? "order-2"
@@ -13,18 +13,21 @@ export default function NavMenu() {
           ? "order-1"
           : "order-2";
         console.log("IconClasses(navList.icon)", IconClasses(navList.icon));
+        const centeredIcon =
+          index === 1 ? "border-r-2 border-l-2 rder-2 border-gray-300 px-6" : "";
         return (
-          <li className="flex items-center mx-2" key={index}>
+          <li className={`flex items-center mx-2 ${centeredIcon}`} key={index}>
             {navList.icon && (
               <FontAwesomeIcon
-                className={`${iconPosition} w-1/5`}
-                size="1x"
+                className={`${iconPosition}`}
+                size="2x"
+                color="#2266CC"
                 icon={IconClasses(navList.icon)}
               />
             )}
             {navList.title && (
               <div
-                className={` w-4/5 span flex flex-col order-2 ${textPosition}`}
+                className={`w-4/5 span flex flex-col order-2 ${textPosition} mx-2`}
               >
                 <p className="text-sm">{navList?.title}</p>
                 <h6 className="font-bold">{navList?.text}</h6>
